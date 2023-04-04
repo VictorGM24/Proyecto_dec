68167ad1c50893cc000a3ad4e0a81c08d1f9381c
94victorgm@gmail.com <94victorgm@gmail.com>
2023-04-04 08:53:39 +0000

    FIN DEL PROYECTO

--- models/marts/marca/fct_marca.sql
+++ models/marts/marca/fct_marca.sql
@@ -8,9 +8,6 @@ with
             cliente,
             count(marca) as cantidad_total_vendida_por_marca,
             sum(venta) as importe_total_marca
-            --avg(venta) as importe_medio_pedido_cliente,
-           -- max(venta) as importe_pedido_max,
-           -- min(venta) as importe_pedido_min
 
         from ventasnoviembre
 
